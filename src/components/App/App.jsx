7b6c912da18519c80d88050a2eb839140fb0ce56
OVECJOE <ovecjoe123@gmail.com>
2023-10-07 22:16:54 +0100

    Made sidebar responsive

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,19 @@
-import Header from "../Header/Header"
-import Sidebar from "../Sidebar/Sidebar"
+import Header from "../Header/Header";
+import Sidebar from "../Sidebar/Sidebar";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-        <div className='App'>
-            <Header />
-            <main>
-                <Sidebar />
-                <div>
-                    {/* Poems */}
-                </div>
-            </main>
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="App">
+                <Header />
+                <main>
+                    <Sidebar />
+                    <div>{/* Poems */}</div>
+                </main>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,55 +1,59 @@
-import Button from "../Button/Button"
-import "./Sidebar.css"
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+import Button from "../Button/Button";
+import "./Sidebar.css";
+import { categories } from "../../assets/constants";
 
 const Sidebar = () => {
-  const categories = [
-    "All",
-    "Love",
-    "Nature",
-    "Life",
-    "Sadness",
-    "Friendship",
-    "Happiness",
-    "Hope",
-    "Faith",
-    "Family",
-    "Religion",
-    "Time",
-    "Beauty",
-    "Death",
-    "Peace",
-    "War",
-  ]
+    const [showSidebar, setShowSidebar] = useState(true);
 
-  return (
-    <aside className="app-sidebar">
-      <div className="menu-list">
-        <h4>Categories</h4>
-        <div className="box categories-box">
-            {categories.map((category, index) => {
-                return (
-                    <Button
-                        key={index}
-                        text={category}
-                        type="button"
-                        style={{
-                            padding: '0.5rem 1rem',
-                            border: 'none',
-                            borderRadius: '5px',
-                        }}
-                    />
-                )
-            })}
-        </div>
-      </div>
-      <div className="menu-list">
-        <h4>Filters</h4>
-        <div className="box filters-box">
-            <p className="empty-text">No Filters Available</p>
-        </div>
-      </div>
-    </aside>
-  );
+    return (
+        <>
+            <aside className={`app-sidebar ${showSidebar ? "" : "closed"}`}>
+                <div className="menu-list">
+                    <h4>Categories</h4>
+                    <div className="box categories-box">
+                        {categories.map((category, index) => {
+                            return (
+                                <Button
+                                    key={index}
+                                    text={category}
+                                    type="button"
+                                    style={{
+                                        padding: "0.5rem 1rem",
+                                        border: "none",
+                                        borderRadius: "5px",
+                                    }}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="menu-list">
+                    <h4>Filters</h4>
+                    <div className="box filters-box">
+                        <p className="empty-text">No Filters Available</p>
+                    </div>
+                </div>
+            </aside>
+            <div
+                className="sidebar-toggler"
+                aria-label="Sidebar Menu"
+                onClick={() => setShowSidebar(!showSidebar)}
+            >
+                {showSidebar ? (
+                    <i className="fa-solid fa-arrow-left"></i>
+                ) : (
+                    <i className="fa-solid fa-arrow-right"></i>
+                )}
+            </div>
+        </>
+    );
 };
 
-export default Sidebar
+Sidebar.propTypes = {
+    show: PropTypes.bool,
+};
+
+export default Sidebar;
